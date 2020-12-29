451ba91ea789671663b44dda532114bc95d4a456
Dave Fury <david.fury@reservoir-devs.com>
2020-12-29 12:16:35 +0000

    feat: added service collection extensions

---
+++ src/ReservoirDevs.ServiceCollection.Extensions/ApiVersioningExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ReservoirDevs.ServiceCollection.Extensions
+{
+    public static class ApiVersioningExtensions
+    {
+        public static IServiceCollection AddApiVersioning(this IServiceCollection serviceCollection, IConfigurationSection configurationSection)
+        {
+            return serviceCollection.AddApiVersioning(configurationSection.Bind);
+        }
+
+        public static IServiceCollection AddVersionedApiExplorer(this IServiceCollection serviceCollection, IConfigurationSection configurationSection)
+        {
+            return serviceCollection.AddVersionedApiExplorer(configurationSection.Bind);
+        }
+    }
+}
