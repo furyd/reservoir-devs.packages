403763285e1c34e843ee7f50e7ab3f1810ce892e
Dave Fury <david.fury@reservoir-devs.com>
2020-12-29 12:17:23 +0000

    feat: added reflection test helper code

---
+++ src/ReservoirDevs.Test.Helpers/ReflectionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReservoirDevs.Test.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static TOutput GetField<TInput, TOutput>(TInput input, string fieldName)
+        {
+            var field = typeof(TInput).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (field == null)
+            {
+                throw new Exception($"{fieldName} not found");
+            }
+
+            return (TOutput)field.GetValue(input);
+        }
+
+        public static TOutput GetProperty<TInput, TOutput>(TInput input, string fieldName)
+        {
+            var field = typeof(TInput).GetProperty(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (field == null)
+            {
+                throw new Exception($"{fieldName} not found");
+            }
+
+            return (TOutput)field.GetValue(input);
+        }
+
+        public static MethodInfo GetMethod<TInput>(string methodName)
+        {
+            var method = typeof(TInput).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static);
+            
+            return method ?? throw new Exception($"{methodName} not found");
+        }
+    }
+}
