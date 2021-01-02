a5be9177e48571d56e980b3c9b2e17df77f709ad
Dave Fury <david.fury@reservoir-devs.com>
2021-01-02 14:28:45 +0000

    feat: added new pass-through extension for when acting as a proxy

---
+++ src/ReservoirDevs.Controllers/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+
+namespace ReservoirDevs.Controllers.Extensions
+{
+    public static class ControllerBaseExtensions
+    {
+        public static async Task PassThrough(this ControllerBase controllerBase, HttpResponseMessage message)
+        {
+            foreach (var (key, value) in message.Content.Headers)
+            {
+                controllerBase.Response.Headers.Add(key, new StringValues(value.ToArray()));
+            }
+
+            controllerBase.Response.StatusCode = (int)message.StatusCode;
+
+            await message.Content.CopyToAsync(controllerBase.Response.Body);
+        }
+    }
+}
