b793c704a7d75b8aa1df6a397f3ead7a921e5a0e
Dave Fury <david.fury@reservoir-devs.com>
2021-01-02 14:22:10 +0000

    feat: added extension method for aggregating enumerated strings

---
+++ src/ReservoirDevs.Logging/Extensions/IEnumerableStringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ReservoirDevs.Logging.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableStringExtensions
+    {
+        public static string ToString(this IEnumerable<string> values, string separator) => string.Join(separator, values);
+    }
+}
